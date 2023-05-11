2ec99692d62950188858d3146aff1faa3cbd130c
provishalk <provishalk@gmail.com>
2023-05-11 10:05:10 +0530

    okta SSO ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { SecureRoute, Security, LoginCallback } from '@okta/okta-react';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
+import Home from './components/Home';
+import Protected from './components/Protected';
 
-function App() {
+const oktaAuth = new OktaAuth({
+  issuer: `https://${process.env.REACT_APP_OKTA_DOMAIN}/oauth2/default`,
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  redirectUri: window.location.origin + '/login/callback'
+});
+
+const App = () => {
+  const history = useHistory();
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <Route path='/' exact={true} component={Home} />
+      <SecureRoute path='/protected' component={Protected} />
+      <Route path='/login/callback' component={LoginCallback} />
+    </Security>
   );
-}
+};
 
-export default App;
+const AppWithRouterAccess = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWithRouterAccess;
